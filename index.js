d163a3d9f7be7e86e943463a3932aa363509ac1d
chanrabose <chandrabosep3112@gmail.com>
2022-10-24 01:14:40 +0530

    changed

--- index.js
+++ index.js
@@ -3,16 +3,16 @@
 
 
 
-    var textArea = document.querySelector("#text");
+    var textArea = document.querySelector("#text-area");
     //Returns the element which matches id #text and stores in pText
-    var btnClicked = document.querySelector('#btn');
+    var btnClicked = document.querySelector('#generate-btn');
     //Returns the element which matches id #btn and stores in pBtn
     
 
     //pBtn will listen for any clicks and calls the function 
     btnClicked.addEventListener("click",function(){
     //sends random elements from the set
-    var len = tweets[Math.floor(Math.random()*tweets.length)];
-    textArea.innerHTML = len;
+    var randomTweet = tweets[Math.floor(Math.random()*tweets.length)];
+    textArea.innerHTML = randomTweet;
     });
 
