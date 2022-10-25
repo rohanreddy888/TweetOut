16e05048f2b2d40fe2f741e1aeac10549b572a84
chandrabosep <chandrabosep3112@gmail.com>
2022-10-25 21:45:20 +0530

    Changed

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var cpyBtn = document.querySelector(".copy");
+var twitterBtn = document.querySelector(".twitter")
+
 
     var textArea = document.querySelector("#text-area");
     //Returns the element which matches id #text and stores in pText
@@ -6,9 +9,22 @@
     
 
     //pBtn will listen for any clicks and calls the function 
+
+  textArea.innerHTML = '<i class="fa-solid fa-heart"></i>';
+
     btnClicked.addEventListener("click",function(){
     //sends random elements from the set
     var randomTweet = tweets[Math.floor(Math.random()*tweets.length)];
     textArea.innerHTML = randomTweet;
     });
 
+
+    
+    cpyBtn.addEventListener("click",function(){
+        navigator.clipboard.writeText(textArea.innerHTML);
+    });
+
+    twitterBtn.addEventListener("click",function(){
+        var tweetUrl = "https://twitter.com/intent/tweet?url=" + textArea.innerHTML ;
+        window.open(tweetUrl,"_blank");
+    });
